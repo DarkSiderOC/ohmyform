2ce611fb7e71b904de4f2b9b1ee4ce8ba7b257d6
Alberto <darksider96@gmail.com>
2020-06-21 16:46:16 +0200

    [Prerelease] Bumped version number to 2.18.0

--- gulpfile.js
+++ gulpfile.js
@@ -71,11 +71,12 @@ function commitChanges(cb) {
         .pipe(git.add())
         .pipe(git.commit(`[Prerelease] Bumped version number to ${packageJson.version}`))
         .on('end', function () {
+            console.log(git.status())
             console.log("ok")
-            git.push('origin', 'master', function(err) {
-                if (err) throw (err);
-                cb(err)
-              });
+            // git.push('origin', 'master', function(err) {
+            //     if (err) throw (err);
+            //     cb(err)
+            //   });
         })
         // .pipe(git.push('origin', 'master', (err) => {
         //     console.log("dio poroc")
